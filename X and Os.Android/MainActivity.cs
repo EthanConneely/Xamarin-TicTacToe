19fc1ba46639dc98b103ee6ad354dc22e9ac23ee
Ethan Conneely <Econn50@outlook.com>
2021-10-18 19:01:16 +0100

    Fixed andoid button

--- X and Os.Android/MainActivity.cs
+++ X and Os.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace X_and_Os.Droid
 {
-    [Activity(Label = "X_and_Os", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "X and Os", Icon = "@drawable/x", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
